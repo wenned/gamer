e8f18a762509215c2d3f85d8c495e689b7338343
Wenned <wenned@gmail.com>
2020-04-10 21:57:14 -0300

    resolução

--- settings.py
+++ settings.py
@@ -1,15 +1,15 @@
 class Settings():
     
     def __init__(self):
-        self.screen_w = 800
-        self.screen_h = 600
+        self.screen_w = 900
+        self.screen_h = 700
         self.bg = (230,230,230)
         self.ship_speed = 1.5
 
         #   configuração dos projeteis
          
         self.bullet_speed_factor = 2
-        self.bullet_width = 4
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 90, 60, 70
         self.bullets_allowed = 3      
